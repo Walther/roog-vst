c7b16f0ae9bdaf32960a58a87f3b4cd940d5ad13
Veeti Haapsamo <veeti.haapsamo@gmail.com>
2018-10-11 01:51:49 -0700

    Update to support newest roog with ADSR. Regress to monophonic synth

--- src/lib.rs
+++ src/lib.rs
@@ -16,45 +16,13 @@ fn midi_pitch_to_freq(pitch: u8) -> f64 {
   ((f64::from(pitch as i8 - A4_PITCH)) / 12.).exp2() * A4_FREQ
 }
 
-#[derive(Copy, Clone, Debug)]
-struct Note {
-  on: bool,
-  duration: f64,
-}
-
-impl Default for Note {
-  fn default() -> Note {
-    Note {
-      on: false,
-      duration: 0.0,
-    }
-  }
-}
-
-struct Keyboard {
-  notes: [Note; 128],
-}
-
-impl Default for Keyboard {
-  fn default() -> Keyboard {
-    Keyboard {
-      notes: [Note::default(); 128],
-    }
-  }
-}
-
 struct RoogVST {
   sample_rate: f64,
   time: f64,
-  keyboard: Keyboard,
   synth: roog::MonoSynth,
 }
 
 impl RoogVST {
-  fn time_per_sample(&self) -> f64 {
-    1.0 / self.sample_rate
-  }
-
   fn process_midi_event(&mut self, data: [u8; 3]) {
     match data[0] {
       128 => self.note_off(data[1]),
@@ -64,13 +32,17 @@ impl RoogVST {
   }
 
   fn note_on(&mut self, note: u8) {
-    self.keyboard.notes[note as usize].duration = 0.0;
-    self.keyboard.notes[note as usize].on = true;
+    let hertz = midi_pitch_to_freq(note);
+    self.synth.note_on(hertz)
   }
 
   fn note_off(&mut self, note: u8) {
-    self.keyboard.notes[note as usize].duration = 0.0;
-    self.keyboard.notes[note as usize].on = false;
+    let hertz = midi_pitch_to_freq(note);
+    self.synth.note_off(hertz)
+  }
+
+  fn time_per_sample(&self) -> f64 {
+    1.0 / self.sample_rate
   }
 }
 
@@ -78,7 +50,6 @@ impl Default for RoogVST {
   fn default() -> RoogVST {
     RoogVST {
       sample_rate: 44100.0,
-      keyboard: Keyboard::default(),
       time: 0.0,
       synth: roog::MonoSynth::new(),
     }
@@ -94,7 +65,7 @@ impl Plugin for RoogVST {
       category: Category::Synth,
       inputs: 2,
       outputs: 2,
-      parameters: 4,
+      parameters: 8,
       initial_delay: 0,
       ..Info::default()
     }
@@ -118,6 +89,11 @@ impl Plugin for RoogVST {
       1 => "Sin",
       2 => "Square",
       3 => "Triangle",
+
+      4 => "Attack",
+      5 => "Decay",
+      6 => "Sustain",
+      7 => "Release",
       _ => "",
     }.to_string()
   }
@@ -133,28 +109,18 @@ impl Plugin for RoogVST {
 
   fn set_sample_rate(&mut self, rate: f32) {
     self.sample_rate = f64::from(rate);
+    self.synth.set_sample_rate(f64::from(rate));
   }
 
   fn process(&mut self, buffer: &mut AudioBuffer<f32>) {
     let samples = buffer.samples();
-
     let per_sample = self.time_per_sample();
 
     for (input_buffer, output_buffer) in buffer.zip() {
       let mut time = self.time;
 
       for (_, output_sample) in input_buffer.iter().zip(output_buffer) {
-        let mut mix = 0.0f64;
-
-        for (index, current_note) in self.keyboard.notes.iter_mut().enumerate() {
-          if current_note.on == true {
-            let hertz = midi_pitch_to_freq(index as u8);
-            let value = self.synth.get_sample(hertz, time);
-
-            mix += value;
-            current_note.duration += per_sample;
-          }
-        }
+        let mut mix = self.synth.get_sample(time);
 
         *output_sample = mix as f32;
         time += per_sample;
